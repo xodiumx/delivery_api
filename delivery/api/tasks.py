b37f1786d0be53858e266acb8bfdc4dce776de28
Maksim Alekseev <am.xlexeev@gmail.com>
2023-05-26 22:16:10 +0500

    update_locations task - 20x faster

--- delivery/api/tasks.py
+++ delivery/api/tasks.py
@@ -1,6 +1,8 @@
+from random import randint
+
 from delivery.celery import app
 
-from .models import Car, random_location
+from .models import Car, Location
 
 
 @app.task
@@ -9,6 +11,12 @@ def update_locations_of_all_cars():
     Функция обновления локаций у всех автомобилей, запускается каждые 3 мин.
     """
     cars = Car.objects.all()
+
+    locations = Location.objects.all()
+    last_location_id = locations[len(locations) - 1].id
+
     for car in cars:
-        car.current_location = random_location()
+        random_id = randint(1, last_location_id)
+        car.current_location = locations[random_id]
+
     Car.objects.bulk_update(cars, ('current_location',))
