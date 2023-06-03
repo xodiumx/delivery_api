e002ae8c3c352127be276b142262ab800553c3d1
Maksim Alekseev <am.xlexeev@gmail.com>
2023-06-03 13:36:55 +0500

    add two services CargoService and CarService

--- delivery/api/serializers.py
+++ delivery/api/serializers.py
@@ -1,18 +1,17 @@
 from collections import OrderedDict
 
-from django.shortcuts import get_object_or_404
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from .exceptions import SameValueException
-from .models import Car, Cargo, Location
-from .utils import calculate_count_of_cars, get_info_about_cars
+from .models import Car, Cargo
+from .utils import (CargoService, CarService, calculate_count_of_cars,
+                    get_info_about_cars)
 
 
 class CargoToRepresentation:
 
     def to_representation(self, instance: Cargo) -> OrderedDict:
         """
-        Для ответа меняем id на zip_index
+        Для ответа меняем id на full_address
         """
         data = super().to_representation(instance)
         data['pick_up'] = instance.pick_up.get_full_address()
@@ -51,50 +50,13 @@ class CargoCreateSerializer(CargoToRepresentation, ModelSerializer):
         fields = ('pick_up', 'delivery_to', 'weight', 'description')
 
     def validate(self, data: OrderedDict) -> OrderedDict:
-        """
-        - Достаем значения 'pick_up' и 'delivery' из не десериализованной data
-        - Проверки if not, если используем update без этих полей
-        """
-        pick_up = self.initial_data.get('pick_up')
-        delivery_to = self.initial_data.get('delivery_to')
-
-        data['pick_up'] = pick_up
-        data['delivery_to'] = delivery_to
-
-        if not pick_up:
-            data.pop('pick_up')
-
-        if not delivery_to:
-            data.pop('delivery_to')
-
-        if pick_up and pick_up == delivery_to:
-            raise SameValueException(
-                {'detail': 'Одинаковое местоположение груза и доставки'})
-        return data
+        return CargoService.validate_cargo(self, data)
     
     def create(self, validated_data: dict) -> Cargo:
-        """
-        Для создания, проверям есть ли локации с такими индексами в базе.
-        """
-        validated_data['pick_up'] = get_object_or_404(
-            Location, zip_index=self.validated_data.get('pick_up'))
-        validated_data['delivery_to'] = get_object_or_404(
-            Location, zip_index=self.validated_data.get('delivery_to'))
-        return Cargo.objects.create(**validated_data)
+        return CargoService.create_cargo(self, validated_data)
 
     def update(self, instance: Cargo, validated_data: dict) -> Cargo:
-        """
-        Для обновления груза, берем новое значение из validated_data,
-        если его нет берем старое из instance.
-        """
-        instance.pick_up = validated_data.get('pick_up', instance.pick_up)
-        instance.delivery_to = validated_data.get(
-            'delivery_to', instance.delivery_to)
-        instance.weight = validated_data.get('weight', instance.weight)
-        instance.description = validated_data.get(
-            'description', instance.description)
-        instance.save()
-        return instance
+        return CargoService.update_cargo(self, instance, validated_data)
     
 
 class CarSerializer(ModelSerializer):
@@ -102,26 +64,14 @@ class CarSerializer(ModelSerializer):
     class Meta:
         model = Car
         fields = ('plate', 'current_location', 'load_capacity')
-    
+
     def validate(self, data: OrderedDict) -> OrderedDict:
         """Достаем значения 'current_location' из не десериализованной data."""
         data['current_location'] = self.initial_data.get('current_location')
         return data
 
-    def update(self, instance: Car, validated_data: dict):
-        """
-        Для обновления груза, берем новое значение из validated_data,
-        если его нет берем старое из instance.
-        """
-        current_location = validated_data.get('current_location')
-        if current_location:
-            instance.current_location = get_object_or_404(
-                Location, zip_index=current_location)
-        instance.plate = validated_data.get('plate', instance.plate)
-        instance.load_capacity = validated_data.get(
-            'load_capacity', instance.load_capacity)
-        instance.save()
-        return instance
+    def update(self, instance: Car, validated_data: dict) -> Car:
+        return CarService.update_car(self, instance, validated_data)
     
     def to_representation(self, instance: Car) -> OrderedDict:
         """
--- delivery/api/utils.py
+++ delivery/api/utils.py
@@ -1,8 +1,79 @@
+from collections import OrderedDict
+
+from django.shortcuts import get_object_or_404
 from geopy import distance
 
-from .models import Car, Cargo
+from .exceptions import SameValueException
+from .models import Car, Cargo, Location
 
 DISTANCE_DIFF = 450
+
+
+class CargoService:
+
+    def validate_cargo(self, data: OrderedDict) -> OrderedDict:
+        """
+        - Достаем значения 'pick_up' и 'delivery' из не десериализованной data
+        - Проверки if not, если используем update без этих полей
+        """
+        pick_up = self.initial_data.get('pick_up')
+        delivery_to = self.initial_data.get('delivery_to')
+
+        data['pick_up'] = pick_up
+        data['delivery_to'] = delivery_to
+
+        if not pick_up:
+            data.pop('pick_up')
+
+        if not delivery_to:
+            data.pop('delivery_to')
+
+        if pick_up and pick_up == delivery_to:
+            raise SameValueException(
+                {'detail': 'Одинаковое местоположение груза и доставки'})
+        return data
+
+    def create_cargo(self, validated_data: dict) -> Cargo:
+        """
+        Для создания, проверям есть ли локации с такими индексами в базе.
+        """
+        validated_data['pick_up'] = get_object_or_404(
+            Location, zip_index=self.validated_data.get('pick_up'))
+        validated_data['delivery_to'] = get_object_or_404(
+            Location, zip_index=self.validated_data.get('delivery_to'))
+        return Cargo.objects.create(**validated_data)
+
+    def update_cargo(self, cargo: Cargo, validated_data: dict) -> Cargo:
+        """
+        Для обновления груза, берем новое значение из validated_data,
+        если его нет берем старое из instance.
+        """
+        cargo.pick_up = validated_data.get('pick_up', cargo.pick_up)
+        cargo.delivery_to = validated_data.get(
+            'delivery_to', cargo.delivery_to)
+        cargo.weight = validated_data.get('weight', cargo.weight)
+        cargo.description = validated_data.get(
+            'description', cargo.description)
+        cargo.save()
+        return cargo
+
+
+class CarService:
+
+    def update_car(self, car: Car, validated_data: dict) -> Car:
+        """
+        Для обновления груза, берем новое значение из validated_data,
+        если его нет берем старое из instance.
+        """
+        current_location = validated_data.get('current_location')
+        if current_location:
+            car.current_location = get_object_or_404(
+                Location, zip_index=current_location)
+        car.plate = validated_data.get('plate', car.plate)
+        car.load_capacity = validated_data.get(
+            'load_capacity', car.load_capacity)
+        car.save()
+        return car
 
 
 def calculate_count_of_cars(cargo: Cargo) -> int:
